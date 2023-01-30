c831163eeb0d0b1495af43ddc7817b043c3fcb45
Samir Cambraia <cambraia_samir@hotmail.com>
2023-01-30 11:19:52 -0300

    teste SALARIO 1008

---
+++ desafio-beecrowd/1008.dart
@@ -0,0 +1,11 @@
+//SALARIO
+import 'dart:io';
+
+void main() {
+  int num1 = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!);
+  double num3 = double.parse(stdin.readLineSync()!);
+
+  print("NUMBER = ${num1}");
+  print("SALARY = U\$ ${(num2 * num3).toStringAsFixed(2)}");
+}//end main
